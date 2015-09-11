9efd9f4379d68d98785ac744f8c68dd183da70e7
Asaf Yishai <asaf@yishai.info>
2015-09-11 18:14:01 +0300

    Add coverage tests

--- gulpfile.js
+++ gulpfile.js
@@ -4,16 +4,27 @@ var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')();
 
 gulp.task('lint', function () {
-	return gulp.src(['gulpfile.js', './src/*.js', './test/*.js'])
-		.pipe(plugins.eslint())
-		.pipe(plugins.eslint.format('stylish'));
+	return gulp.
+		src(['gulpfile.js', './src/**/*.js', './test/**/*.js']).
+		pipe(plugins.eslint()).
+		pipe(plugins.eslint.format('stylish'));
 });
 
-gulp.task('test', function () {
-	return gulp.src('test/test.js', {read: false})
-		.pipe(plugins.mocha({reporter: 'nyan'}));
+gulp.task('test', function (cb) {
+	return gulp.
+		src(['src/**/*.js']).
+		pipe(plugins.istanbul()).
+		pipe(plugins.istanbul.hookRequire()).
+		on('finish', function () {
+			return gulp.
+				src(['test/*.js']).
+				pipe(plugins.mocha({reporter: 'nyan'})).
+				pipe(plugins.istanbul.enforceThresholds({thresholds: {global: 90}})).
+				once('error', function () {
+					process.exit(1);
+				}).
+				once('end', cb);
+		});
 });
 
-gulp.task('default', function () {
-
-});
+gulp.task('default', ['lint', 'test']);
